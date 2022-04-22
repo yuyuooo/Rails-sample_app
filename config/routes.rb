de9379064404a9e820408f88990084c126401532
yuyuooo <103011542+yuyuooo@users.noreply.github.com>
2022-04-22 05:24:50 +0000

    [Finish]3-1~10

---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+
+  get 'top' => 'homes#top'
+  resources :lists
+  # Fror details on the DSL avaiilable within this file, see https://guides.rubyonrails.org/routing.hend
+end
