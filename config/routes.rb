ef17882b1b075897823ed96580ecfa95d08772cb
yuyuooo <103011542+yuyuooo@users.noreply.github.com>
2022-05-11 05:14:04 +0000

    [Add]RSpecテスト作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
-  get 'top' => 'homes#top'
+  get "/top" => "homes#top"
+
   resources :lists
   # Fror details on the DSL avaiilable within this file, see https://guides.rubyonrails.org/routing.hend
 end
